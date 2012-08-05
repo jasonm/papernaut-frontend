aa5be159eaa68a0fb574ac1f68d435767f593b5d
Jason Morrison <jason.p.morrison@gmail.com>
2012-08-05 19:31:01 +0200

    Use extracted omniauth-zotero gem

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'rails', '3.2.7'
 gem 'pg'
 gem 'omniauth'
 gem 'omniauth-oauth'
-
+gem 'omniauth-zotero'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,58 +1,5 @@
-require 'omniauth-oauth'
-
-module OmniAuth
-  module Strategies
-    class Zotero < OmniAuth::Strategies::OAuth
-      option :name, "zotero"
-
-      option :client_options, {
-        :site => 'https://www.zotero.org',
-        :request_token_path => '/oauth/request',
-        :access_token_path => '/oauth/access',
-        :authorize_path => '/oauth/authorize',
-        :scheme => :query_string
-      }
-
-      uid do
-        raw_info['userID'].to_i
-      end
-
-      info do
-        {
-          'name' => raw_info['username']
-        }
-      end
-
-      credentials do
-        {
-          'token' => raw_info['oauth_token'],
-          'secret' => raw_info['oauth_token_secret']
-        }
-      end
-
-      extra do
-        {
-          'raw_info' => raw_info
-        }
-      end
-
-      private
-
-      def raw_info
-        access_token.params
-      end
-
-      def callback_phase
-        session['oauth'][name.to_s]['callback_confirmed'] = true
-        super
-      end
-    end
-  end
-end
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   zotero_key = '40b254ff45835ab31cdf'
   zotero_secret = 'e32947c055c489eda7b4'
   provider :zotero, zotero_key, zotero_secret
-  # Started GET "/auth/zotero/callback?oauth_token=5d241f60fc0c7f159078&oauth_verifier=06d1f411fc1a267ceb5b" for 127.0.0.1 at 2012-08-03 22:53:21 +0200
 end

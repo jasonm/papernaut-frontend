1db866e491413ca27134fe798228d13a49ff5faa
Jason Morrison <jason.p.morrison@gmail.com>
2012-08-05 19:47:08 +0200

    start to spike ZoteroClient

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'pg'
 gem 'omniauth'
 gem 'omniauth-oauth'
 gem 'omniauth-zotero'
+gem 'faraday'
+gem 'ratom', require: 'atom'
 
 # Gems used only for assets and not required
 # in production environments by default.
---
+++ lib/zotero_client.rb
@@ -0,0 +1,30 @@
+require 'faraday'
+require 'atom'
+
+module ZoteroClient
+  class User
+    attr_reader :uid, :key
+
+    def initialize(uid, key)
+      @uid, @key = uid, key
+    end
+
+    def items
+      Item.for_user(self)
+    end
+  end
+
+  class Item
+    def self.for_user(user)
+      url = "https://api.zotero.org/users/#{user.uid}/items?key=#{user.key}&format=atom&content=json"
+      response = Faraday.get(url)
+      feed = Atom::Feed.load_feed(response.body)
+      content_hashes = feed.entries.map { |entry| JSON.parse(entry.content) }
+      content_hashes.map { |hash| self.new(hash) }
+    end
+
+    def initialize(attributes)
+      @attributes = attributes
+    end
+  end
+end

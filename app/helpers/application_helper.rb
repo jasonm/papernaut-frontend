48139eed2d0a901ad543961024bb37e120e6568a
Jason Morrison <jason.p.morrison@gmail.com>
2012-08-15 00:24:03 +0200

    User can connect 2nd service, kinda

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
       ""
     end
   end
+
+  def source_sentence(user)
+    sources = user.articles.select(:source).map(&:source).uniq
+    sources << "empty" if sources.empty?
+
+    sources.map(&:capitalize).to_sentence
+  end
 end

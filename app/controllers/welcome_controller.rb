3dc71a3fc6e490115961f0c89577151f7f717e7b
Jason Morrison <jason.p.morrison@gmail.com>
2012-08-14 23:38:33 +0200

    only assign if signed in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @discussions = Discussion.of_articles(current_user.articles)
-    @number_of_articles_without_discussions = current_user.articles.count - @discussions.count
+    if current_user
+      @discussions = Discussion.of_articles(current_user.articles)
+      @number_of_articles_without_discussions = current_user.articles.count - @discussions.count
+    end
   end
 end

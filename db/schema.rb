d19311a08ce228082287ba5aa4670f26330c39e1
Jason Morrison <jason.p.morrison@gmail.com>
2012-08-19 22:34:55 +0200

    Add users unconfirmed_email:string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120819194512) do
+ActiveRecord::Schema.define(:version => 20120819203412) do
 
   create_table "articles", :force => true do |t|
     t.integer  "user_id"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20120819194512) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
 end

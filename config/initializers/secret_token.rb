f89539400afae42f0b5421d4f54d89b32dc384a9
Jason Morrison <jason.p.morrison@gmail.com>
2013-01-04 23:50:53 -0800

    Require overridden secret token in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PapernautFrontend::Application.config.secret_token = '02ee2ba5e845fd8014233dad346ed121213553c79add864375b59c4122c8c820402b42602c67a147716464e1086a8d20076f242c636ed7518c50899d640deec7'
+PapernautFrontend::Application.config.secret_token = if Rails.env.production?
+  ENV['SECRET_TOKEN'] || raise("Set ENV['SECRET_TOKEN'] for production")
+else
+  '02ee2ba5e845fd8014233dad346ed121213553c79add864375b59c4122c8c820402b42602c67a147716464e1086a8d20076f242c636ed7518c50899d640deec7'
+end

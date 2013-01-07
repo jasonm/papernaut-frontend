63db7ac12cc38eb4c72b4a5af210ee59556705cf
Jason Morrison <jason.p.morrison@gmail.com>
2013-01-07 15:42:23 -0800

    Remove number_of_articles_without_discussions

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -7,7 +7,6 @@ class DiscussionsController < ApplicationController
 
     begin
       @discussions = Discussion.of_articles(current_user.articles)
-      @number_of_articles_without_discussions = current_user.articles.count - @discussions.count
     rescue Discussion::EngineUnreachableException => e
       flash.now[:alert] = "Discussion matching is temporarily unavailable."
     end
